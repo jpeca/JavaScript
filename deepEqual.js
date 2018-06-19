b572c754f96de94a63d8adff1ee6c4e03c4937f1
Predrag Jovanovic <predrag.jovanovic@gowi.rs>
2018-06-19 12:50:24 +0200

    Add files via upload

---
+++ deepEqual.js
@@ -0,0 +1,16 @@
+function deepEqual (item1, item2){
+	// Uporedi direktno i ako su jednaki true
+	if (item1 === item2) return true;
+    // Ako nisu objekti  onda je false 
+    if (item1 == null || typeof item1 != "object" ||
+      item2 == null || typeof item2 != "object") return false;
+    // Kad jesu objekti uporedi properties listu
+	let keys1 = Object.keys(item1), keys2 = Object.keys(item2);
+    if (keys2.length != keys2.length) return false;
+	// Kad su iste dužine uporedi properti po properti - rekurzija
+    for (let key of keys1) {
+      if (!keys2.includes(key) || !deepEqual(item1[key], item2[key])) return false;
+    }
+
+   return true;
+}
